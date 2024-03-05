9dbd40265dbe445be511246450f5cd34b81bf28a
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 21:55:18 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -23,6 +23,30 @@ export default function Weather() {
           </div>
         </div>
       </form>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearFix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly cloudy"
+              className="float-left"
+            ></img>
+            <span className="temperature">{Math.round(temperature)}</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
