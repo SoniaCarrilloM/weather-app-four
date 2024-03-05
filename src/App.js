31659c29463cf6e85203fa104a3763680abfd608
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 21:49:38 +0100

    added react and weather files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://github.com/SoniaCarrilloM"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sonia Carrillo{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/SoniaCarrilloM/weather-app-two-week-4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
